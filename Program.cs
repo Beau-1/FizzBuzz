0d7851d7db6a7a44e07a85c3e9df9e0ce64b637e
Beau <jamesbuchanan0528@gmail.com>
2022-12-30 14:28:05 -0600

    FizzBuzz Exercise

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        public static string FizzBuzz(int number)
+        {
+            var answer = "";
+
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                answer = "FizzBuzz";
+            }
+
+            else if (number % 3 == 0)
+            {
+                answer = "Fizz";
+            }
+
+            else if(number % 5 == 0)
+            {
+                answer = "Buzz";
+            }
+
+            return answer;
+
+        }
+        static void Main(string[] args)
+        {
+            string result = FizzBuzz(5); 
+
+
+
+            Console.WriteLine(result);
+            
+
+        }
+    }
+}
